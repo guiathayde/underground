20b1679ded00eb527f4d67386a4f7e3e547d9027
etoremt@gmail.com <etoremt@gmail.com>
2021-07-29 16:44:38 -0300

    update

--- Level.cpp
+++ Level.cpp
@@ -37,8 +37,14 @@ void Level::Initialize(int enemiesNum)
 
   if (plataformTexture.loadFromFile("assets/background/Plataforms/teste.png"))
     printf("Carregou plataforma\n");
-  Platform *auxPlatform = new Platform(&plataformTexture, sf::Vector2f(5000.0f, 200.0f), sf::Vector2f(100.0f, 600.0f));
+  
+  Platform *auxPlatform = new Platform(&plataformTexture, sf::Vector2f(800.0f, 200.0f), sf::Vector2f(100.0f, 600.0f));
+  Platform *auxPlatform2 = new Platform(&plataformTexture, sf::Vector2f(2000.0f, 200.0f), sf::Vector2f(300.0f, 400.0f));
+  Platform *auxPlatform3 = new Platform(&plataformTexture, sf::Vector2f(100.0f, 1000.0f), sf::Vector2f(200.0f, 300.0f));
+
   this->platforms.push_back(auxPlatform);
+  this->platforms.push_back(auxPlatform2);
+  this->platforms.push_back(auxPlatform3);
 }
 
 void Level::CheckCollison()
